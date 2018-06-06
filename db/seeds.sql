072dbfa35513a507e9839f581ef1db05cdcd3d70
Gabe Maughan <gmwccgolf@gmail.com>
2018-06-06 16:41:26 -0400

    wed 6/6

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Big Boy');
+INSERT INTO burgers (burger_name) VALUES ('Carolina Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwest Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Queso Burger');
+INSERT INTO burgers (burger_name) VALUES ('Onion Madness');
