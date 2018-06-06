39ca16d7f963ca1019ed82f9dd8f5e58821d3467
gsmaughan <gmwccgolf@gmail.com>
2018-06-06 18:12:48 -0400

    wednesday 6/6

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+
 var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
